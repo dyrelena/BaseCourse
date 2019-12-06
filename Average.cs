12afb768c144b52ef029e9ff4841fe264f1ae0c1
dyrelena <57507904+dyrelena@users.noreply.github.com>
2019-12-07 00:10:50 +0200

    Add files via upload

--- Average.cs
+++ Average.cs
@@ -9,27 +9,20 @@ namespace Average
             int arrSize = 5;
             int[] nums = new int[arrSize];
             int sum = 0;
-            Random rnd = new Random();
+            Console.WriteLine("Введите {0} чисел:", arrSize);
             int value;
             
             for (int j = 0; j < arrSize; j++)
             {
-                value = rnd.Next();
+                value = Convert.ToInt32(Console.ReadLine());
                 nums[j] = value;
-            }
-
-         foreach (int j in nums)
-            {
-                Console.WriteLine(j);
             }
 
             for (int j = 0; j < arrSize; j++)
             {
                 sum += nums[j];
             }
-
-
-            Console.Clear();
+                                   
             Console.WriteLine("Среднее арифметическое = " + sum/arrSize);
             Console.ReadLine();
         }
